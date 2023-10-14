efc4a77a4770ee5d20c780b8fc32978bfa130880
tshubham75 <tshubham75@gmail.com>
2023-10-14 21:07:32 +0530

    Make changes in the application.rb file for deploying the project in the production environment.

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,11 @@ module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    # to diploy on production it must be correct
+    config.active_record.sqlite3_production_warning = falsegg
     # Configuration for the application, engines, and railties goes here.
-    #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
